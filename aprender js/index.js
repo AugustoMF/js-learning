eb03c19d69b01f2067c278900bbe8f9895ac33a0
AugustoMF <114231094+AugustoMF@users.noreply.github.com>
2022-10-10 19:32:12 -0300

    Add files via upload

---
+++ aprender js/index.js
@@ -0,0 +1,31 @@
+const nome = document.getElementById ('nomeid').value
+const idade = document.getElementById('idadeid').value
+const time = document.getElementById('timeid').value
+
+function getElementById (botao){
+    console.log (nome)
+    console.log (idade)
+    console.log (time)
+}
+
+function botao () {
+    console.log (nome)
+    console.log (idade)
+    console.log (time)
+
+if (team == 'steelers' && age >= 18) {
+     ('Here we go, have a beer')
+} 
+
+else if (team == steelers && age < 18) {
+    ('Here we go, no beer for ya')
+}
+
+else if (team != 'steelers' && age >= 18) {
+    ('have a beer, but fuck you')
+}
+else if (team != 'steelers' && age < 18) {
+    ('just fuck you and no beer for you too.')
+}
+
+}
